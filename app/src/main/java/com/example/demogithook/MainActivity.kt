d14c5a797f6561f6c3cab893a9b29a4f1b9c7b6e
thuandd <thuandd@apero.vn>
2024-03-19 00:21:13 +0700

    - test commit not formatted .kt file

--- app/src/main/java/com/example/demogithook/MainActivity.kt
+++ app/src/main/java/com/example/demogithook/MainActivity.kt
@@ -13,7 +13,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right
+
+                , systemBars.   bottom)
             insets
         }
     }
